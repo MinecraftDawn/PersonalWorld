f43dfa44c1a75b1a46fd25ada3f6fcc7b54d4686
MinecraftDawn <eric4902077@gmail.com>
2018-04-19 20:18:20 +0800

    Fixed a thread waining on create world

---
+++ src/create/world/PersonalWorldCreator.java
@@ -0,0 +1,20 @@
+package create.world;
+
+import org.bukkit.WorldCreator;
+
+public class PersonalWorldCreator implements Runnable{
+	
+	private WorldCreator world;
+	
+	public PersonalWorldCreator(WorldCreator world) {
+		this.world = world;
+	}
+
+	@Override
+	public void run() {
+		try{
+			world.createWorld();
+		}catch (Exception e) {
+		}
+	}
+}
