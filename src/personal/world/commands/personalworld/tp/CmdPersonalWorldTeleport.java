ffb18261009ffc302e5dcf4a1cfa065c8d1332da
MinecraftDawn <eric4902077@gmail.com>
2018-04-22 00:37:52 +0800

    Rename

---
+++ src/personal/world/commands/personalworld/tp/CmdPersonalWorldTeleport.java
@@ -0,0 +1,15 @@
+package personal.world.commands.personalworld.tp;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import personal.world.commands.IPersonalCommand;
+
+public class CmdPersonalWorldTeleport implements IPersonalCommand{
+
+	@Override
+	public void run(CommandSender sender, Command cmd, String[] args) {
+		
+	}
+
+}
