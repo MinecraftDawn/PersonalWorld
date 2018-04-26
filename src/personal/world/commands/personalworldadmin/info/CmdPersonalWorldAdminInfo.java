23782094e6808c6d6bdb92fbb7f05f3d4e2ca52f
MinecraftDawn <eric4902077@gmail.com>
2018-04-27 00:04:31 +0800

    Create CmdPersonalWorldAdminInfo class

---
+++ src/personal/world/commands/personalworldadmin/info/CmdPersonalWorldAdminInfo.java
@@ -0,0 +1,15 @@
+package personal.world.commands.personalworldadmin.info;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import personal.world.commands.IPersonalCommand;
+
+public class CmdPersonalWorldAdminInfo implements IPersonalCommand{
+
+	@Override
+	public void run(CommandSender sender, Command cmd, String[] args) {
+		
+	}
+
+}
