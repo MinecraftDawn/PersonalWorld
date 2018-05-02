73b4c9372e06fdc84a0edb917cb4435c9ab79a6e
MinecraftDawn <eric4902077@gmail.com>
2018-05-02 21:58:32 +0800

    Remove unuse import

--- src/personal/world/commands/personalworld/CmdPersonalWorld.java
+++ src/personal/world/commands/personalworld/CmdPersonalWorld.java
@@ -5,9 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
-import personal.world.PersonalWorld;
 import personal.world.commands.IPersonalCommand;
 import personal.world.file.manager.FileManager;
 
