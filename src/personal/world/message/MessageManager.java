57d91d31c75786d78d6137baa3eea418bb532f57
MinecraftDawn <eric4902077@gmail.com>
2018-05-05 14:01:35 +0800

    MessageManager

---
+++ src/personal/world/message/MessageManager.java
@@ -0,0 +1,14 @@
+package personal.world.message;
+
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+
+public class MessageManager {
+	private Plugin plugin;
+	
+	private MessageManager instance;
+	
+	private MessageManager() {
+		
+	}
+
+}
